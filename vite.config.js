e6585eec45711568d6681783720f847f2ef4f723
isabella920 <61594645+isabella920@users.noreply.github.com>
2026-04-03 18:35:26 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
-
 
 export default defineConfig({
   // 這裡的名稱必須跟你的 GitHub 儲存庫名稱完全一樣 (例如 life-system-lite)
